a4e6488eedd817b2ae163381fa6648153e4f050a
core <cornet.remi@gmail.com>
2023-12-18 22:53:20 -0500

    Routes Tests

---
+++ src/test/scala/com/corem/jobsboard/http/routes/JobRoutesSpec.scala
@@ -0,0 +1,125 @@
+package com.corem.jobsboard.http.routes
+
+import cats.effect.*
+import cats.implicits.*
+import io.circe.generic.auto.*
+import org.http4s.circe.CirceEntityCodec.*
+import cats.effect.testing.scalatest.AsyncIOSpec
+import org.scalatest.freespec.AsyncFreeSpec
+import org.scalatest.matchers.should.Matchers
+
+import org.http4s.*
+import org.http4s.dsl.*
+import org.http4s.implicits.*
+import org.typelevel.log4cats.Logger
+import org.typelevel.log4cats.slf4j.Slf4jLogger
+import java.util.UUID
+
+import com.corem.jobsboard.core.*
+import com.corem.jobsboard.fixtures.*
+import com.corem.jobsboard.domain.job.*
+
+class JobRoutesSpec
+    extends AsyncFreeSpec
+    with AsyncIOSpec
+    with Matchers
+    with Http4sDsl[IO]
+    with JobFixture {
+
+  val jobs: Jobs[IO] = new Jobs[IO] {
+    override def create(ownerEmail: String, jobInfo: JobInfo): IO[UUID] =
+      IO.pure(NewJobUuid)
+
+    override def all(): IO[List[Job]] =
+      IO.pure(List(AwesomeJob))
+
+    override def find(id: UUID): IO[Option[Job]] =
+      if (id == AwesomeJobUuid)
+        IO.pure(Some(AwesomeJob))
+      else
+        IO.pure(None)
+
+    override def update(id: UUID, jobInfo: JobInfo): IO[Option[Job]] =
+      if (id == AwesomeJobUuid)
+        IO.pure(Some(UpdatedAwesomeJob))
+      else
+        IO.pure(None)
+
+    override def delete(id: UUID): IO[Int] =
+      if (id == AwesomeJobUuid) IO.pure(1)
+      else IO.pure(0)
+  }
+
+  given logger: Logger[IO]      = Slf4jLogger.getLogger[IO]
+  val jobRoutes: HttpRoutes[IO] = JobRoutes[IO](jobs).routes
+
+  "JobRoutes" - {
+    "should return a job with a given id" in {
+      for {
+        response <- jobRoutes.orNotFound.run(
+          Request(method = Method.GET, uri = uri"/jobs/843df718-ec6e-4d49-9289-f799c0f40064")
+        )
+        retrieved <- response.as[Job]
+      } yield {
+        response.status shouldBe Status.Ok
+        retrieved shouldBe AwesomeJob
+      }
+    }
+
+    "should return all jobs" in {
+      for {
+        response <- jobRoutes.orNotFound.run(
+          Request(method = Method.POST, uri = uri"/jobs")
+        )
+        retrieved <- response.as[List[Job]]
+      } yield {
+        response.status shouldBe Status.Ok
+        retrieved shouldBe List(AwesomeJob)
+      }
+    }
+
+    "should create a new job" in {
+      for {
+        response <- jobRoutes.orNotFound.run(
+          Request(method = Method.POST, uri = uri"/jobs/create").withEntity(AwesomeJob.jobInfo)
+        )
+        retrieved <- response.as[UUID]
+      } yield {
+        response.status shouldBe Status.Created
+        retrieved shouldBe NewJobUuid
+      }
+    }
+
+    "should only update a job that exists" in {
+      for {
+        responseOk <- jobRoutes.orNotFound.run(
+          Request(method = Method.PUT, uri = uri"/jobs/843df718-ec6e-4d49-9289-f799c0f40064")
+            .withEntity(UpdatedAwesomeJob.jobInfo)
+        )
+        responseInvalid <- jobRoutes.orNotFound.run(
+          Request(method = Method.PUT, uri = uri"/jobs/843df718-ec6e-4d49-9289-000000000000")
+            .withEntity(UpdatedAwesomeJob.jobInfo)
+        )
+      } yield {
+        responseOk.status shouldBe Status.Ok
+        responseInvalid.status shouldBe Status.NotFound
+      }
+    }
+
+    "should only delete a job that exists" in {
+      for {
+        responseOk <- jobRoutes.orNotFound.run(
+          Request(method = Method.DELETE, uri = uri"/jobs/843df718-ec6e-4d49-9289-f799c0f40064")
+            .withEntity(UpdatedAwesomeJob.jobInfo)
+        )
+        responseInvalid <- jobRoutes.orNotFound.run(
+          Request(method = Method.DELETE, uri = uri"/jobs/843df718-ec6e-4d49-9289-000000000000")
+            .withEntity(UpdatedAwesomeJob.jobInfo)
+        )
+      } yield {
+        responseOk.status shouldBe Status.Ok
+        responseInvalid.status shouldBe Status.NotFound
+      }
+    }
+  }
+}
